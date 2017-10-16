9ca843f6515a41142d6308322ce6b014abc8b6f0
bleung329 <bleung@stuy.edu>
2017-10-15 22:36:34 -0400

    init

---
+++ db_builder.py
@@ -0,0 +1,26 @@
+#!usr/bin/python
+'''
+Brian Leung
+PD 7 SoftDev
+
+'''
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+
+f=""
+
+db = sqlite3.connect(f) #open if f exists, otherwise create
+c = db.cursor()    #facilitate db ops
+
+#==========================================================
+#INSERT YOUR POPULATE CODE IN THIS ZONE
+
+
+command = ""          #put SQL statement in this string
+c.execute(command)    #run SQL statement
+
+#==========================================================
+db.commit() #save changes
+db.close()  #close database
