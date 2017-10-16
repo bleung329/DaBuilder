50236bf602603f812a28eacf62add2b23345f32c
bleung329 <bleung@stuy.edu>
2017-10-16 06:25:06 -0400

    Will finish

--- db_builder.py
+++ db_builder.py
@@ -8,16 +8,19 @@ PD 7 SoftDev
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
-
-f=""
-
+csvfile = open("testcase.csv",'rb')
+reader = csv.reader(csvfile)
+redRow = reader.next()
+pr
+("")
+redRo[0] = 
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
-
+000
 command = ""          #put SQL statement in this string
 c.execute(command)    #run SQL statement
 
