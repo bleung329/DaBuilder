7484a44bbd90bf6c05643c54f6753f056336a4fc
bleung329 <bleung@stuy.edu>
2017-10-16 23:24:03 -0400

    fixed

--- db_builder.py
+++ db_builder.py
@@ -2,27 +2,34 @@
 '''
 Brian Leung
 PD 7 SoftDev
-
+(
 '''
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
+f = "dab.db"
+command = ""
 
-csvfile = open("testcase.csv",'rb')
-reader = csv.reader(csvfile)
-redRow = reader.next()
-pr
-("")
-redRo[0] = 
+csvfile = open("peeps.csv",'rb')
+reader = csv.DictReader(csvfile)
+
+command = 'CREATE TABLE peeps(code TEXT, mark INTEGER, id INTEGER)'
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
+c.execute(command)
+for row in reader:
+	command = 'INSERT INTO peeps VALUES("%s",%s,%s)' % (row['name'], row['age'], row['id'])
+	c.execute(command)
 
-#==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
+csvfile = open("courses.csv","rb")
+reader = csv.DictReader(csvfile)
+command = 'CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)'
+c.execute(command)
+for row in reader:
+	command = 'INSERT INTO courses VALUES("%s",%s,%s)' % (row['code'], row['mark'], row['id'])
+	c.execute(command)
 
-000
-command = ""          #put SQL statement in this string
-c.execute(command)    #run SQL statement
+#c.execute(command)    #run SQL statement
 
 #==========================================================
 db.commit() #save changes
